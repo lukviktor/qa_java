644f53f4e1e80e370438d09e59e043f9630d8e20
Viktor Lukashev <lukashevviktor@gmail.com>
2023-03-23 10:39:31 +0300

    add LionParameterizedTest in LionTest

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,6 +1,4 @@
-
 import com.example.Feline;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,10 +1,13 @@
 import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -62,4 +65,37 @@ public class LionTest {
         Assert.assertEquals(expectedText, exception.getMessage());
     }
 
+    @RunWith(Parameterized.class)
+    public static class LionParameterizedTest {
+        private String lionSex;
+        private boolean hasManeExpected;
+
+        @Mock
+        Feline feline;
+
+        public LionParameterizedTest(String lionSex, boolean hasManeExpected){
+            this.lionSex = lionSex;
+            this.hasManeExpected = hasManeExpected;
+        }
+        @Before
+        public void SetUp() {
+            MockitoAnnotations.initMocks(this);
+        }
+        @Parameterized.Parameters
+        public static Object[][] getDataForm() {
+            return new Object[][] {
+                    {"Самец", true},
+                    {"Самка", false},
+            };
+        }
+
+        @Test
+        public void maleHaveManyReturnsExpectedValue() throws Exception {
+            Lion lion = new Lion(lionSex, feline);
+            Assert.assertEquals("Используйте допустимые значения пола животного - самец или самка",
+                    hasManeExpected, lion.doesHaveMane());
+
+        }
+
+    }
 }
